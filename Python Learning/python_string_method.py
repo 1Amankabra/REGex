8cbe054195ee7c801f6cc9b7e33581071889589a
amanmahesh <108971073+amanmahesh@users.noreply.github.com>
2022-07-15 15:58:52 +0530

    commit

--- Python Learning/python_string_method.py
+++ Python Learning/python_string_method.py
@@ -7,3 +7,16 @@ y = 5
 total = x + y
 total
 print(total)
+
+
+
+first_name = 'aman'
+last_name = 'kabra'
+
+print(first_name.count('1'))
+print(first_name.isascii())
+print(first_name.isdigit())
+print(last_name.capitalize())
+print(first_name.isnumeric())
+print(first_name.find('m'))
+print(first_name.removeprefix())
