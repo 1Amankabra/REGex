bfa0310cc1750464899883e48197ac37041b4c8d
amanmahesh <108971073+amanmahesh@users.noreply.github.com>
2022-07-16 13:23:31 +0530

    commit

--- Python Learning/python_string_method.py
+++ Python Learning/python_string_method.py
@@ -1,22 +1,73 @@
-from tkinter import Y
-
-
-x = 3
-y = 5
-
-total = x + y
-total
-print(total)
 
 
 
-first_name = 'aman'
-last_name = 'kabra'
+# x = 3
+# y = 5
 
-print(first_name.count('1'))
-print(first_name.isascii())
-print(first_name.isdigit())
-print(last_name.capitalize())
-print(first_name.isnumeric())
-print(first_name.find('m'))
-print(first_name.removeprefix())
+# total = x + y
+# total
+# print(total)
+
+
+
+# first_name = 'aman'
+# last_name = 'kabra'
+
+# print(first_name.count('1'))
+# print(first_name.isascii())
+# print(first_name.isdigit())
+# print(last_name.capitalize())
+# print(first_name.isnumeric())
+# print(first_name.find('m'))
+# print(first_name.removeprefix())
+
+# sentence = f'{first_name} {last_name} is coder'
+# print(sentence)
+
+# lst = [1,2,3,4,'hii',['a','b']]
+# print(lst[0])
+# print(lst[-1])
+# print(lst[-2][0])
+
+
+# lst = [1,2,3,4,'hello',['a','b']]
+# random_number_list = [10,33,4,22,76,90,23,87]
+# random_number_list.sort()
+# print(random_number_list)
+
+# print(random_number_list,'before appending anything.')
+# random_number_list.append("HELLO")
+# print(random_number_list,'after appending anything.')
+
+# ls = 'aman'
+# s = list(ls)
+# print(s)
+# s.sort()
+# print(s)
+
+# a = [1,3,5]
+# b = [6,8,9]
+# print(a + b)
+# a.append(b)
+# print(a)
+# a.extend(b)
+# print(a)
+
+# a.pop('')
+
+# a[0:len(a):1]
+
+# st  = set([1,2,3,4,5,5])
+# print(st)
+# st = {1,3,5,2,8,6,8}
+# print(st)
+
+# bt = {4,5,6}
+# print(type(st))
+
+# print(st.union(bt))
+# print(st.intersection(bt))
+# print(st.difference(bt))
+
+
+tup = 
