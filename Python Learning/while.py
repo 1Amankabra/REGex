a02c2c47dd5a75b035f8399e3ee5ebb37abe5e33
amanmahesh <108971073+amanmahesh@users.noreply.github.com>
2022-07-17 13:09:44 +0530

    commit

---
+++ Python Learning/while.py
@@ -0,0 +1,36 @@
+# i = int(1)
+# while i<= 10:
+#     print(i)
+#     i = i+1
+
+
+fname = 'aman'
+last = 'kabra'
+
+# if fname=='aman' and last=='maheshwari':
+#     print('yes')
+# else:
+#     print('no')
+
+    
+# if fname=='aman' or last=='maheshwari':
+#     print('yes')
+# else:
+#     print('no')
+
+
+# if fname=='aman' or not last=='maheshwari':
+#     print('yes')
+# else:
+#     print('no')
+
+if 'aman' in ['a','b','c']:
+    print('aman in not string')
+elif 'p' in 'aman':
+    print('p in not in aman')
+elif 'a' in 'aman':
+    print('a in aman')
+else:
+    print('no')
+
+ 
